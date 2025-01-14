a107879cff6bfba5197624a3d6ff7921a2db02e3
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-01-14 23:02:30 +0100

    Add code to generate random hex color surrounded by hashtag box, add dependencies (chalk & randomColor)

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 // import dependencies
 import { argv } from 'node:process';
+import { stringify } from 'node:querystring';
+import boxen from 'boxen';
 import chalk from 'chalk';
+import Box from 'cli-box';
 import { randomColor } from 'randomcolor';
 
-console.log(randomColor({ luminosity: argv[2], hue: argv[3] }));
+//define random color and chalk for formatting output color
+const color = randomColor();
+const chalkColor = chalk.hex(color);
+
+const userColor = randomColor({ luminosity: argv[2], hue: argv[3] });
+const chalkUserColor = chalk.hex(userColor);
+
+//if argv.length is over 3, values for luminosity and hue are pulled from argv[2] & argv[3], otherwise a random color is generated
+if (argv.length > 3) {
+  console.log(
+    chalkUserColor(`
+ ###############################
+ ###############################
+ ###############################
+ #####                     #####
+ #####       ${userColor}       #####
+ #####                     #####
+ ###############################
+ ###############################
+ ###############################`),
+  );
+} else {
+  console.log(
+    chalkColor(`
+ ###############################
+ ###############################
+ ###############################
+ #####                     #####
+ #####       ${color}       #####
+ #####                     #####
+ ###############################
+ ###############################
+ ###############################`),
+  );
+}
