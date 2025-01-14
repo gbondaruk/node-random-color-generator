9e1de11f49cf9ae83e8a1d1ee617d8d82ea99133
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-01-14 23:15:08 +0100

    fix dependencies & preflight issues

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import { argv } from 'node:process';
 import chalk from 'chalk';
 import { randomColor } from 'randomcolor';
 
-//define random color and chalk for formatting output color
+// define random color and chalk for formatting output color
 const color = randomColor();
 const chalkColor = chalk.hex(color);
 
 const userColor = randomColor({ luminosity: argv[2], hue: argv[3] });
 const chalkUserColor = chalk.hex(userColor);
 
-//if argv.length is over 3, values for luminosity and hue are pulled from argv[2] & argv[3], otherwise a random color is generated
+// if argv.length is over 3, values for luminosity and hue are pulled from argv[2] & argv[3], otherwise a random color is generated
 if (argv.length > 3) {
   console.log(
     chalkUserColor(`
